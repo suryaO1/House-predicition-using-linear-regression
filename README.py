1c715916571ab7c0c1d7546be43f73acd7c7a72e
suryaO1 <78307588+suryaO1@users.noreply.github.com>
2022-01-12 23:14:54 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,2 @@
+# House-predicition-using-linear-regression
+house price prediction using linear regression algorithm, in thes project i did the preproceesing ,feature scaling and feature selection using correlaton-coffection then i did linear regression algorithm
